602065935e6eb0e3f873b545d029b01e6c1f6522
mcooper7649 <55610025+mcooper7649@users.noreply.github.com>
2021-01-15 17:37:30 -0500

    Completed Dice Game Challenge

---
+++ Dicee Challenge - Starting Files/index.js
@@ -0,0 +1,36 @@
+// Create the Randomness to specifications
+// Then Attach that randomness to your image
+// Link Randomimage to source folder
+// Create variables for the images using querySelectorAll and Array Structure
+// Set our new Random Image using setAttribute format
+
+var randomNumber1 = Math.floor((Math.random() * 6) + 1);  //Random Number Generator 1-6
+var randomNumber2 = Math.floor((Math.random() * 6) + 1);  //Random Number Generator 1-6
+
+
+var randomDiceImage = "dice" + randomNumber1 + ".png";  // Generate a Random Image using randomNumber1 and source;
+
+var randomImageSource = "images/" + randomDiceImage; // Our source for Image1
+
+var randomDiceImage2 = "dice" + randomNumber2 + ".png"; // Generate a Random Image using randomNumber 2 and source;
+
+var randomImageSource2 = "images/" + randomDiceImage2; // Our source for image2
+
+
+
+var image1 = document.querySelectorAll("img")[0];  
+var image2 = document.querySelectorAll("img")[1];
+
+image1.setAttribute("src", randomImageSource)
+image2.setAttribute("src", randomImageSource2)
+
+var winner = document.querySelector("h1");
+
+
+if (randomNumber1 > randomNumber2){
+    winner.innerHTML = "Player 1 Wins";
+} else if (randomNumber2 > randomNumber1){
+    winner.innerHTML = "Player 2 Wins";
+} else {
+    winner.innerHTML = "Draw";
+}

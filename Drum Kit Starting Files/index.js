b1383104e4661cc5d58d248c0a2d41f5e64b00d0
mcooper7649 <55610025+mcooper7649@users.noreply.github.com>
2021-01-16 14:25:43 -0500

    Completed background image styles

--- Drum Kit Starting Files/index.js
+++ Drum Kit Starting Files/index.js
@@ -9,7 +9,8 @@ var numOfDrumButtons = document.querySelectorAll(".drum").length
 for (var i = 0; i < numOfDrumButtons; i++){
     
     document.querySelectorAll(".drum")[i].addEventListener("click", function(){
-        alert("i got clicked");
+        var audio = new Audio ('/Drum Kit Starting Files/sounds/tom-1.mp3');
+        audio.play(); ;
     });
 
 }
